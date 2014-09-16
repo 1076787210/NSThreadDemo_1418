4554b1434ef44ed3d61d53025b7747e1957dad8d
zhangcheng <zhangcheng@zhangchengdeMacBook-Pro.local>
2014-09-16 16:16:48 +0800

    Initial Commit

---
+++ NSThreadDemo_1418Tests/NSThreadDemo_1418Tests.m
@@ -0,0 +1,34 @@
+//
+//  NSThreadDemo_1418Tests.m
+//  NSThreadDemo_1418Tests
+//
+//  Created by zhangcheng on 14-9-16.
+//  Copyright (c) 2014年 zhangcheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSThreadDemo_1418Tests : XCTestCase
+
+@end
+
+@implementation NSThreadDemo_1418Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
